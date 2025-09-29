7ebfaf0d7a0bc148f13c30b4d8ce71a437e710d1
Angelo <angeloma2080@gmail.com>
2025-09-29 02:35:12 -0500

    Conexión a base de datos

--- backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/LePettiteCoffeApplication.java
+++ backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/LePettiteCoffeApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) // LA ANOTACION ES TEMPORAL HASTA TENER BD
+@SpringBootApplication // (exclude = { DataSourceAutoConfiguration.class }) // LA ANOTACION ES TEMPORAL
+						// HASTA TENER BD
 public class LePettiteCoffeApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Repository/TemporalDAO.java
+++ backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Repository/TemporalDAO.java
@@ -3,6 +3,7 @@ package com.cursoIntegrador.lePettiteCoffe.Repository;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
@@ -12,8 +13,10 @@ import com.cursoIntegrador.lePettiteCoffe.Model.User;
 @Repository
 public class TemporalDAO implements IUserDAO {
     private final Map<String, User> users = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
 
-    public TemporalDAO(PasswordEncoder passwordEncoder) {
+    public TemporalDAO(PasswordEncoder passwordEncoder, JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
         users.put("admin", new User("admin", passwordEncoder.encode("1234")));
         users.put("user", new User("user", passwordEncoder.encode("abcd")));
     }
@@ -26,5 +29,16 @@ public class TemporalDAO implements IUserDAO {
     @Override
     public void save(User user) {
         users.put(user.getUsername(), user);
+
+        // Borrar si no se quiere usar la BD
+        String sql = """
+                    INSERT INTO Cuenta (Email, Password, Rol, Estado, FechaRegistro)
+                    VALUES (?, ?, ?, ?, NOW())
+                """;
+        jdbcTemplate.update(sql,
+                user.getUsername(),
+                user.getPassword(),
+                "CLIENTE",
+                "ACTIVO");
     }
 }
