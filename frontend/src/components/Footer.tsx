37c79c913229fb186a6362073ae02c745290a7aa
YasinRomero <U23220351@utp.edu.pe>
2025-09-28 19:56:21 -0500

    Auth: Sistema de Login, Register y Logout completados + Pestaña Perfil terminada

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,40 +1,40 @@
 import "./Footer.css";
 
 function Footer(): React.ReactNode {
-  return (
-    <footer className="footer">
-      <article className="footer__links">
-        <section>
-          <h6>Sobre nosotros</h6>
-          <ul>
-            <li>
-              <a href="#">Misión y Visión</a>
-            </li>
-            <li>
-              <a href="#">Reseñas</a>
-            </li>
-            <li>
-              <a href="#">Contáctanos</a>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h6>Soporte</h6>
-          <ul>
-            <li>
-              <a href="#">Reporte</a>
-            </li>
-            <li>
-              <a href="#">Preguntas frecuentes</a>
-            </li>
-          </ul>
-        </section>
-      </article>
-      <section className="footer__copyright">
-        Le Pettite Coffee @ 2025. Todos los derechos reservados
-      </section>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <article className="footer__links">
+                <section>
+                    <h6>Sobre nosotros</h6>
+                    <ul>
+                        <li>
+                            <a href="#">Misión y Visión</a>
+                        </li>
+                        <li>
+                            <a href="#">Reseñas</a>
+                        </li>
+                        <li>
+                            <a href="#">Contáctanos</a>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h6>Soporte</h6>
+                    <ul>
+                        <li>
+                            <a href="#">Reporte</a>
+                        </li>
+                        <li>
+                            <a href="#">Preguntas frecuentes</a>
+                        </li>
+                    </ul>
+                </section>
+            </article>
+            <section className="footer__copyright">
+                Le Pettite Coffee @ 2025. Todos los derechos reservados
+            </section>
+        </footer>
+    );
 }
 
 export default Footer;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,38 +1,40 @@
 import { Link } from "react-router";
 import "./Header.css";
 
-function Header(): React.ReactNode {
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <h1 className="m-0">LE PETTITE COFFEE</h1>
-      </div>
-      <nav className="header__nav">
-        <ul>
-          <li>
-            <Link to="/inicio">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/menus">Menús</Link>
-          </li>
-          <li>
-            <Link to="/servicios">Servicios</Link>
-          </li>
-          <li>
-            <Link to="/reseñas">Reseñas</Link>
-          </li>
-        </ul>
-      </nav>
-      <section className="header__actions">
-        <Link className="link-outline" to="/shoppingcart">
-          <i className="fa-solid fa-cart-shopping"></i>
-        </Link>
-        <Link className="link-outline" to="/login">
-          UNIRME
-        </Link>
-      </section>
-    </header>
-  );
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__logo">
+                <h1>
+                    <Link to="/">LE PETTITE COFFEE</Link>
+                </h1>
+            </div>
+            <nav className="header__nav">
+                <ul>
+                    <li>
+                        <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="/menus">Menús</Link>
+                    </li>
+                    <li>
+                        <Link to="/servicios">Servicios</Link>
+                    </li>
+                    <li>
+                        <Link to="/reseñas">Reseñas</Link>
+                    </li>
+                </ul>
+            </nav>
+            <section className="header__actions">
+                <Link className="link-outline" to="/shoppingcart">
+                    <i className="fa-solid fa-cart-shopping"></i>
+                </Link>
+                <Link className="link-outline" to="/login">
+                    UNIRME
+                </Link>
+            </section>
+        </header>
+    );
 }
 
 export default Header;
