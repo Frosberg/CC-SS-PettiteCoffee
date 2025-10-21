e2760865256bf87c95cfee1d79f7386674cd77b2
Angelo <angeloma2080@gmail.com>
2025-10-20 20:56:37 -0500

    Implementación de test tautologico que prueba la ejecución del flujo para probar el funcionamiento de mockito

--- backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Controller/AuthController.java
+++ backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.cursoIntegrador.lePettiteCoffe.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoIntegrador.lePettiteCoffe.Model.LoginRequest;
---
+++ backend/src/test/java/com/cursoIntegrador/lePettiteCoffe/Service/AuthServiceTest.java
@@ -0,0 +1,54 @@
+package com.cursoIntegrador.lePettiteCoffe.Service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.cursoIntegrador.lePettiteCoffe.Model.IUserDAO;
+import com.cursoIntegrador.lePettiteCoffe.Model.User;
+import com.cursoIntegrador.lePettiteCoffe.Security.AuthService;
+import com.cursoIntegrador.lePettiteCoffe.Security.JwtUtil;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private IUserDAO userDAO;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private AuthService authService;
+
+    private User mockUser;
+
+    @BeforeEach
+    void setUp() {
+        mockUser = new User("usuarioPrueba", "contraseñaEncriptada");
+    }
+
+    @Test
+    void testLogin_Success() {
+        when(userDAO.findByUsername("usuarioPrueba")).thenReturn(mockUser);
+        when(passwordEncoder.matches("contraseñaSinEncriptar", "contraseñaEncriptada")).thenReturn(true);
+        when(jwtUtil.generateToken("usuarioPrueba")).thenReturn("fakeToken");
+
+        String token = authService.login("usuarioPrueba", "contraseñaSinEncriptar");
+
+        assertEquals("fakeToken", token);
+        verify(jwtUtil).generateToken("usuarioPrueba");
+    }
+
+}
