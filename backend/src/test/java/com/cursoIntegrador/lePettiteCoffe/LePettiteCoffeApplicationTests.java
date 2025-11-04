85fcd6341ef1df6952e107f318578d5bdc32dad0
Frosberg <165091658+Frosberg@users.noreply.github.com>
2025-11-04 16:29:26 -0500

    Add SpringBootTest annotation to test class

--- backend/src/test/java/com/cursoIntegrador/lePettiteCoffe/LePettiteCoffeApplicationTests.java
+++ backend/src/test/java/com/cursoIntegrador/lePettiteCoffe/LePettiteCoffeApplicationTests.java
@@ -1,7 +1,9 @@
 package com.cursoIntegrador.lePettiteCoffe;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class LePettiteCoffeApplicationTests {
 
 	@Test
