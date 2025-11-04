5eb36600138985364e518a3bf7bc19e197fb9ab7
Frosberg <165091658+Frosberg@users.noreply.github.com>
2025-11-04 16:47:44 -0500

    Update LePettiteCoffeApplicationTests.java

--- backend/src/test/java/com/cursoIntegrador/lePettiteCoffe/LePettiteCoffeApplicationTests.java
+++ backend/src/test/java/com/cursoIntegrador/lePettiteCoffe/LePettiteCoffeApplicationTests.java
@@ -1,9 +1,7 @@
 package com.cursoIntegrador.lePettiteCoffe;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LePettiteCoffeApplicationTests {
 
 	@Test
