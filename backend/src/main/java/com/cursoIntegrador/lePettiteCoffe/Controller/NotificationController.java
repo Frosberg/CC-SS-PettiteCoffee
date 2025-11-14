f393803e41b5b0b6f77ae9e2ed259fe34d03c21a
Angelo <angeloma2080@gmail.com>
2025-11-13 20:34:28 -0500

    Manejo de seguridad derivado casi en su totalidad a spring security

--- backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Controller/NotificationController.java
+++ backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Controller/NotificationController.java
@@ -1,18 +1,18 @@
 package com.cursoIntegrador.lePettiteCoffe.Controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoIntegrador.lePettiteCoffe.Model.DTO.NotificationDTO;
-import com.cursoIntegrador.lePettiteCoffe.Service.AuthService;
 import com.cursoIntegrador.lePettiteCoffe.Service.DAO.NotificationService;
 
 @RequestMapping("/notificaciones")
@@ -24,20 +24,13 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
 
-    @Autowired
-    private AuthService authService;
-
     @GetMapping("/listar")
-    public ResponseEntity<?> listarNotificaciones(@RequestHeader("Authorization") String authHeader) {
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> listarNotificaciones(Principal principal) {
         try {
-            String token = authHeader.replace("Bearer ", "");
-
-            if (!authService.isTokenValid(token)) {
-                return ResponseEntity.status(401).body("Token inválido o expirado");
-            }
-
-            String email = authService.extractUsername(token);
+            String email = principal.getName();
             logger.info("Buscando notificaciones del email: {}", email);
+
             List<NotificationDTO> notificaciones = notificationService.getAllUserNotis(email);
             logger.info("Notificaciones encontradas: {}", notificaciones.size());
 
@@ -45,7 +38,6 @@ public class NotificationController {
                 return ResponseEntity.noContent().build();
             }
 
-            logger.info("Notificaciones obtenidas para {}", email);
             return ResponseEntity.ok(notificaciones);
 
         } catch (Exception e) {
